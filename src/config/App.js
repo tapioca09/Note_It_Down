da448f3cdfca1413b1d1332a2eace85fe3659042
Amrisha <amrishavardiya09@gmail.com>
2023-06-22 18:41:09 +0530

    new commit

---
+++ src/config/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Components/Header';
+import LandingPage from './Components/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <LandingPage />
+      
+
+      
+    </div>
+  );
+}
+
+export default App;
